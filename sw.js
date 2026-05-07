6c2edd85667c0bd4041601f7d810e3317e24bf6e
roelvangennip-beep <roelvangennip@icloud.com>
2026-05-07 16:11:08 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "liftlog-v5";
+const CACHE = "liftlog-v7";
 const FILES = ["./index.html","./manifest.json","./icon.svg","./config.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
