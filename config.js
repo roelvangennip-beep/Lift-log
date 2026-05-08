6c489179f72016793e2b7f6832d67a5cbedec725
roelvangennip-beep <roelvangennip@icloud.com>
2026-05-08 16:57:51 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Dit bestand hoef je NOOIT te vervangen bij updates!
 // Vul hier eenmalig je Supabase gegevens in.
 
-window.LIFTLOG_URL = "https://qwrympzutcyvlrcrbrxu.supabase.co";
-window.LIFTLOG_KEY = "sb_publishable_ejD5rfbrSyrZALjqngnC3g_TJo4DDci";
+window.LIFTLOG_URL = "JOUW_SUPABASE_URL";   // bijv. https://abcxyz.supabase.co
+window.LIFTLOG_KEY = "JOUW_SUPABASE_KEY";   // jouw publishable key
 window.LIFTLOG_ADMIN = "LIFTLOG-ADMIN";     // verander dit naar jouw eigen geheime admin code
